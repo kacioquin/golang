8e2d1f3da2d09af78e8b12b18480f363affe94f1
Cassio de Freitas <cassio.quintino@gmail.com>
2022-05-13 14:29:03 -0300

    add function with only return

--- 02-function-return/main.go
+++ 02-function-return/main.go
@@ -6,4 +6,8 @@ func main() {
 	n, err := fmt.Println("Hello, I am a function with return")
 	fmt.Println("I'm a function return - ", n)
 	fmt.Println("I'm a error list - ", err)
+
+	result, _ := fmt.Println("Hello, I have only return. The sucess return!")
+	fmt.Println(result)
+
 }
