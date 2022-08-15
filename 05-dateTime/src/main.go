7159b1f14a71b3a0e7885a73d8851dbe628191ac
Cassio de Freitas <cassio.quintino@gmail.com>
2022-08-15 09:29:11 -0300

    Initial commit - workind date type

---
+++ 05-dateTime/src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	currentTime := time.Now()
+	fmt.Println("The time is", currentTime)
+
+	fmt.Println("The year is", currentTime.Year())
+	fmt.Println("The month is", currentTime.Month())
+	fmt.Println("The day is", currentTime.Day())
+	fmt.Println("The hour is", currentTime.Hour())
+	fmt.Println("The minute is", currentTime.Minute())
+	fmt.Println("The seconds is", currentTime.Second())
+
+}
