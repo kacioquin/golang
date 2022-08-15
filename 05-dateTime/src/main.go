82eb1c1fceb52feb54215a46a59b4833d5e338f2
Cassio de Freitas <cassio.quintino@gmail.com>
2022-08-15 09:48:51 -0300

    formating with mt.Printf

--- 05-dateTime/src/main.go
+++ 05-dateTime/src/main.go
@@ -9,11 +9,11 @@ func main() {
 	currentTime := time.Now()
 	fmt.Println("The time is", currentTime)
 
-	fmt.Println("The year is", currentTime.Year())
-	fmt.Println("The month is", currentTime.Month())
-	fmt.Println("The day is", currentTime.Day())
-	fmt.Println("The hour is", currentTime.Hour())
-	fmt.Println("The minute is", currentTime.Minute())
-	fmt.Println("The seconds is", currentTime.Second())
-
+	fmt.Printf("%d-%d-%d %d:%d:%d\n",
+		currentTime.Year(),
+		currentTime.Month(),
+		currentTime.Day(),
+		currentTime.Hour(),
+		currentTime.Minute(),
+		currentTime.Second())
 }
